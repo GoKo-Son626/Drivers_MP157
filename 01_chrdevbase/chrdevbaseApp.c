ff6bbc6ea9ab2400cabd1c3248f3c6818c2e9927
GoKo-Son626 <wangshuqi626@126.com>
2024-11-29 20:43:17 +0800

    [CODE]chrdevbase

--- 01_chrdevbase/chrdevbaseApp.c
+++ 01_chrdevbase/chrdevbaseApp.c
@@ -7,11 +7,7 @@
  */
 #include "stdio.h"
 #include "unistd.h"
-#include "sys/types.h"
-#include "sys/stat.h"
 #include "fcntl.h"
-#include "stdlib.h"
-#include "string.h"
 
 int main(int argc, char *argv[])
 {
