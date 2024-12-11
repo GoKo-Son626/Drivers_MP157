6df39123b6a94bc9069f8ed6e3b28668a72e5131
GoKo-Son626 <wangshuqi626@126.com>
2024-12-11 16:06:53 +0800

    [CODE]TESTED interruptApp.c

--- 09_interrupt/interruptApp.c
+++ 09_interrupt/interruptApp.c
@@ -1,8 +1,8 @@
 /*
  * @Date: 2024-12-01
  * @LastEditors: GoKo-Son626
- * @LastEditTime: 2024-12-04
- * @FilePath: /1-STM32MP157/Drivers_MP157/00_TEMPLATE/driverApp.c
+ * @LastEditTime: 2024-12-11
+ * @FilePath: /1-STM32MP157/Drivers_MP157/09_interrupt/interruptApp.c
  * @Description: 
  */
 #include "stdio.h"
@@ -14,45 +14,37 @@
 #include "string.h"
 
 /*
- * @description		: main主程序
- * @param - argc 	: argv数组元素个数
- * @param - argv 	: 具体参数
- * @return 		: 0 成功;其他 失败
+ * @description		: main
+ * @param - argc 	: Num of argv
+ * @param - argv 	: Specific parameter
+ * @return 		: 0 succeed;other fault
  */
-int main(int argc, char *argv[])
-{
-	int fd, retvalue;
-	char *filename;
-	unsigned char databuf[1];
-	
-	if(argc != 3){
-		printf("Error Usage!\r\n");
-		return -1;
-	}
+int main(int argc, char* argv[]){
+        int ret, fd, key_val;
 
-	filename = argv[1];
+        /* Check the number of arguments */
+        if (argc != 2) {
+                printf("ERROR:argc:%d\n", argc);
+                return -1;
+        }
+        
+        /* Open the device */
+        if((fd = open(argv[1], O_RDONLY)) < 0) {
+                printf("ERROR: %s file open failed!\n", argv[1]);
+                return -1;
+        }
+        
+        /* Read interrupt status */
+        for ( ; ; ) {
+                read(fd, &key_val, sizeof(key_val));    
+                if (key_val == 0) {
+                        printf("ERROR: %s file read failed!\n", argv[1]);
+                } else if (key_val == 1) {
+                        printf("Key Release\n");
+                }
+        }
 
-	/* 打开led驱动 */
-	fd = open(filename, O_RDWR);
-	if(fd < 0){
-		printf("file %s open failed!\r\n", argv[1]);
-		return -1;
-	}
-
-	databuf[0] = atoi(argv[2]);	/* 要执行的操作：打开或关闭 */
-
-	/* 向/dev/led文件写入数据 */
-	retvalue = write(fd, databuf, sizeof(databuf));
-	if(retvalue < 0){
-		printf("LED Control Failed!\r\n");
-		close(fd);
-		return -1;
-	}
-
-	retvalue = close(fd); /* 关闭文件 */
-	if(retvalue < 0){
-		printf("file %s close failed!\r\n", argv[1]);
-		return -1;
-	}
-	return 0;
+        /* Close the device */
+        ret = close(fd);
+        return 0;
 }
